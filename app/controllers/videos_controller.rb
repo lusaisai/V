4f818e3b6e30a393ddcf8f4eeced24ed76f4cba1
lusaisai <lusaisai@163.com>
2014-03-06 00:37:08 +0800

    using leanback player

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,8 @@
-require 'uri'
-
 class VideosController < ApplicationController
   PREFIX = 'http://localhost:8080/videos/'
   def show
     begin
-      video = Video.find params[:id]
-      redirect_to URI.escape( PREFIX + video.path )
+      @video = Video.find params[:id]
     rescue ActiveRecord::RecordNotFound
       redirect_to root_path
     end
