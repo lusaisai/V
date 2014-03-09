e29ea88fc65c75487f52dd1b1277027242298d45
lusaisai <lusaisai@163.com>
2014-03-10 03:02:49 +0800

    create js to process subtitle

--- app/assets/javascripts/video_shortcuts.js
+++ app/assets/javascripts/video_shortcuts.js
@@ -5,12 +5,10 @@
     $(document).keydown( function(e){
         var video = $('video')[0];
         var code = e.which;
-        console.log(code);
-
-        if(video) e.preventDefault();
 
         // space
-        if( code == 32 ) {
+        if( video && code == 32 ) {
+            e.preventDefault();
             if(video.paused) {
                 video.play();
             } else {
@@ -20,26 +18,30 @@
 
         // right arrow
         var progressStep = 10;
-        if( code == 39 ) {
+        if( video && code == 39 ) {
+            e.preventDefault();
             var target = video.currentTime + progressStep;
             video.currentTime =  target <= video.duration ? target : video.duration;
         }
 
         // left arrow
-        if( code == 37 ) {
+        if( video && code == 37 ) {
+            e.preventDefault();
             var target = video.currentTime - progressStep;
             video.currentTime =  target >= 0 ? target : 0;
         }
 
         // up arrow
         var volumeStep = 0.05;
-        if(  code == 38 ) {
+        if( video && code == 38 ) {
+            e.preventDefault();
             var target = video.volume + volumeStep;
             video.volume =  target <= 1 ? target : 1;
         }
 
         //down arrow
-        if(  code == 40 ) {
+        if( video &&  code == 40 ) {
+            e.preventDefault();
             var target = video.volume - volumeStep;
             video.volume =  target >= 0 ? target : 0;
         }
