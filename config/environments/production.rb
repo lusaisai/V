d184c7d22d0a9c8aae4a29b2471a4c966827350a
lusaisai <lusaisai@163.com>
2014-05-28 20:33:02 +0800

    change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ V::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # specific config
-  config.video_dir = '/mnt/hgfs/Videos/'
-  config.base_url = 'http://windowsai/videos/'
-  config.admin_web_socket = 'ws://ubuntus:3001/'
+  config.video_dir = '/mnt/ent/Videos/'
+  config.base_url = 'http://localhost/resources/videos/'
+  config.admin_web_socket = 'ws://localhost:3001/'
 
 end
